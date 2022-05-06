d1755a47f6a76e8b0194376ae9690bb851d1029b
Nicky-Basi <74901671+Nicky-Basi@users.noreply.github.com>
2022-05-06 21:48:03 +0100

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module 'src/app/blockchain.js';
+declare module 'src/app/keygenerator.js';
+declare module 'src/app/main.js';
+declare module 'nickycoin/node_modules/elliptic/lib/elliptic.js';
+declare module 'elliptic'
